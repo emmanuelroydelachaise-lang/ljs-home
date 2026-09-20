07fcd33449b3848a8df52e334eacc25ba7d79e0c
emmanuelroydelachaise-lang <emmanuel.roydelachaise@gmail.com>
2026-09-20 08:53:59 +0200

    Connect LJS Home to central Supabase

--- config.js
+++ config.js
@@ -1,12 +1,8 @@
 window.LJS_CONFIG = {
-  SUPABASE_URL: "https://xrntdqlqkteikknrgvwy.supabase.co",
-  SUPABASE_ANON_KEY: "sb_publishable_jXOFo8ae_aj-YN_nOqG9Tw_vHHoXYrk",
+  SUPABASE_URL: "https://wptorwbmgwsadbuxwyvk.supabase.co",
+  SUPABASE_ANON_KEY: "sb_publishable_VO7mh6MsjflCc8IIUpElPA_8rAaJo89",
   AUTH_PASSWORD_PREFIX: "LJS-",
-  TECH_ACCOUNTS: [
-    { name: "Roy de Lachaise Emmanuel", email: "emmanuel@ljs.local" },
-    { name: "Paterne Adrien", email: "adrien@ljs.local" },
-    { name: "Moro Nabil", email: "nabil@ljs.local" }
-  ],
+  TECH_ACCOUNTS: [],
   ADMIN_ACCOUNT: { name: "Responsable", email: "responsable@ljs.local" }
 };
 
