6696d4f8e25ec12488fe6e5138f2cc14012873bd
emmanuelroydelachaise-lang <emmanuel.roydelachaise@gmail.com>
2026-09-20 09:20:45 +0200

    Mettre à jour la PWA après modification de connexion

--- pwa-clean.js
+++ pwa-clean.js
@@ -1,5 +1,5 @@
 (() => {
-  const VERSION = '20260920-home-uppercase-1';
+  const VERSION = '20260920-hidden-admin-1';
   const CLEAN_KEY = 'ljs_home_pwa_clean_version';
   let deferredPrompt = null;
 
