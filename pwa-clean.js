b6bc2a5c9d039b0c94c1be14238c2e779d12deed
emmanuelroydelachaise-lang <emmanuel.roydelachaise@gmail.com>
2026-09-24 17:29:54 +0200

    Forcer le rafraîchissement PWA pour feuille lundi-vendredi

--- pwa-clean.js
+++ pwa-clean.js
@@ -1,5 +1,5 @@
 (() => {
-  const VERSION = '20260922-chantier-libre-1';
+  const VERSION = '20260924-tech-mon-fri-2';
   const CLEAN_KEY = 'ljs_home_pwa_clean_version';
   let deferredPrompt = null;
 
